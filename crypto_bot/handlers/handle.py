fb966583df3f1f715b0dc809a1f8e32cef5840b0
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-03-01 17:06:40 +0000

    Update handle.py

--- crypto_bot/handlers/handle.py
+++ crypto_bot/handlers/handle.py
@@ -43,7 +43,7 @@ def send_message(ChatjoinRequest,bot:TeleBot):
     file=open('message.txt')
     print(ChatjoinRequest)
     try:
-        bot.approve_chat_join_request(ChatjoinRequest.chat,ChatjoinRequest.from_user)
+        bot.approve_chat_join_request(ChatjoinRequest.chat,ChatjoinRequest.user_chat_id)
     except Exception as e:
         print(e)
     print()
